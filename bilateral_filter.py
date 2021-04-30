4bcf0b18b6e4d8fe7b04f6236e1b888e222ac20c
isLiuHao <603400054@qq.com>
2021-04-30 15:27:55 +0800

    test

--- bilateral_filter.py
+++ bilateral_filter.py
@@ -4,41 +4,32 @@ import math
 import copy
 import time
 
-class BilateralFilter(object):
-    """
-    定义双边滤波器类：通过空间域和值域进行滤波
-    变量：
-        ds：distance sigma
-        rs: range sigma
-        c_weight_table：定义域核
-        s_weight_table: 值域核
-        radius: 模块半径，即模块大小为（2*radius+1）
-    """
+# 双边滤波器类
+class bilateral_filter(object):
+    # 初始化
+    def __init__(self, s_sigma, v_sigma, radius):
+        self.s_sigma = s_sigma          # 空间域sigma
+        self.v_sigma = v_sigma          # 值域sigma
+        self.s_weight_table = []        # 定义域核
+        self.v_weight_table = []        # 值域核
+        self.radius = radius            # 模块半径，即模块大小为（2*radius+1）
 
-    def __init__(self, ds, rs, radiu):
-        """初始化变量"""
-        self.ds = ds
-        self.rs = rs
-        self.c_weight_table = []
-        self.s_weight_table = []
-        self.radius = radiu
-
-    def build_distance_weight_table(self):
-        """定义空间域核"""
+    # 定义空间域核
+    def build_space_weight(self):
         #        size = 2 * self.radius + 1
         for semi_row in range(-self.radius, self.radius + 1):
-            self.c_weight_table.append([])
+            self.s_weight_table.append([])
             for semi_col in range(-self.radius, self.radius + 1):
                 # calculate Euclidean distance between center point and close pixels
-                delta = -(semi_row * semi_row + semi_col * semi_col) / (2 * (self.ds ** 2))
-                self.c_weight_table[semi_row + self.radius].append(
+                delta = -(semi_row * semi_row + semi_col * semi_col) / (2 * (self.s_sigma ** 2))
+                self.s_weight_table[semi_row + self.radius].append(
                     math.exp(delta))
 
-    def build_similarity_weight_table(self):
-        """定义值域核"""
+    # 定义值域核
+    def build_value_weight(self):
         for i in range(256):  # since the color scope is 0 ~ 255
-            delta = -(i * i) / (2 * (self.rs ** 2))
-            self.s_weight_table.append(math.exp(delta))
+            delta = -(i * i) / (2 * (self.v_sigma ** 2))
+            self.v_weight_table.append(math.exp(delta))
 
     def clamp(self, p):
         """return RGB color between 0 and 255"""
@@ -49,16 +40,15 @@ class BilateralFilter(object):
         else:
             return p
 
-    def bilateral_filter(self, src):
-        """ 
+    def start_filter(self, src):
+        """
         双边滤波器的方法：对原始图进行双边滤波，并返回滤波过后的图
         """
-
         height = src.size[0]
         width = src.size[1]
         radius = self.radius
-        self.build_distance_weight_table()
-        self.build_similarity_weight_table()
+        self.build_space_weight()
+        self.build_value_weight()
         in_pixels = src.load()
         raw_data = []
         out_data = copy.deepcopy(src)
@@ -82,16 +72,16 @@ class BilateralFilter(object):
                         tb2 = in_pixels[row_offset, col_offset][2]
                         # 卷积计算
                         cs_red_weight = (
-                                self.c_weight_table[semi_row + radius][semi_col + radius]
-                                * self.s_weight_table[(abs(tr2 - tr))]
+                                self.s_weight_table[semi_row + radius][semi_col + radius]
+                                * self.v_weight_table[(abs(tr2 - tr))]
                         )
                         cs_green_weight = (
-                                self.c_weight_table[semi_row + radius][semi_col + radius]
-                                * self.s_weight_table[(abs(tg2 - tg))]
+                                self.s_weight_table[semi_row + radius][semi_col + radius]
+                                * self.v_weight_table[(abs(tg2 - tg))]
                         )
                         cs_blue_weight = (
-                                self.c_weight_table[semi_row + radius][semi_col + radius]
-                                * self.s_weight_table[(abs(tb2 - tb))]
+                                self.s_weight_table[semi_row + radius][semi_col + radius]
+                                * self.v_weight_table[(abs(tb2 - tb))]
                         )
 
                         cs_sum_red_weight += cs_red_weight
@@ -130,15 +120,16 @@ if __name__ == '__main__':
         radius = input("模块半径设置为：")
         s_sigma = input("空间域sigma设置为：")
         v_sigma = input("值域sigma设置为：")
+        print('双边滤波正在处理........')
         start_time = time.time()
-        bf = BilateralFilter(int(s_sigma), int(v_sigma), int(radius))
-        dest = bf.bilateral_filter(Origin_image)
-        dest.save('BF' + str(i) + '_'+ image_Path)
+        bf = bilateral_filter(int(s_sigma), int(v_sigma), int(radius))
         end_time = time.time()
         timex = str(end_time - start_time)
-
+        dest = bf.start_filter(Origin_image)
+        dest.save('BF' + str(i) + '_' + image_Path)
         plt.subplot(2, 2, i)
         plt.title('s_sigma=' + s_sigma + ',v_sigma=' + v_sigma + ',r=' + radius + ',time=' + timex[0:5], fontsize=20)
         plt.imshow(dest)
         plt.axis('off')
+        print('双边滤波处理结束，处理时间：' + timex[0:5])
     plt.show()
